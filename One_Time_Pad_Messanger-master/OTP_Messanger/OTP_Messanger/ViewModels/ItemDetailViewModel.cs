e4b3ad41471c634bf30d53320aada75d267f223d
Mateusz Bulanda - Gorol <mateuszbulanda-gorol@MacBook-Air-Mateusz.local>
2023-03-22 12:21:12 +0100

    Add init commit

---
+++ One_Time_Pad_Messanger-master/OTP_Messanger/OTP_Messanger/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OTP_Messanger.Models;
+
+namespace OTP_Messanger.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
